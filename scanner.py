92c790e0d823ff63d24ced5e83e39b67715d92b3
Dane Morgan <danemorgan91@gmail.com>
2018-04-25 11:12:28 -0600

    cleaned up

--- scanner.py
+++ scanner.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python
 from nmap import PortScanner
-from os import system
-#run as sudo
-
-#start = input('Define x of 192.168.x.0/24 for first subnet:\t')
-#end = input('Define x of 192.168.x.0/24 for last subnet:\t')
-#file_loc = input('Define path of scan destination\nex. ~/Documents/scan.csv\nPath:\t')
+#run as root
 
 subnet = 192
 
 while subnet <= 255:
 	
 	nm = PortScanner()
-	cidr = '192.168.' + str(subnet) + '.0/24'
-	print('!!! Starting on:',cidr,'!!!')
+	ip_range = '192.168.' + str(subnet) + '.0/24'
+	print('!!! Starting on:',ip_range,'!!!')
 	
 	try:
-		nm.scan(hosts=cidr,arguments='-A -Pn')
+		#nmap -A -Pn 192.168.192.0/24
+		nm.scan(hosts=ip_range, arguments='-A')
 		to_csv = nm.csv()
 
-		print('!!! Successfully scanned',cidr,'!!!')
-		system("beep")
-	
+		print('Successfully scanned',ip_range)
 		print(to_csv)
 		with open('net_scan.csv', 'a+') as f:
 			f.write(to_csv)
 			f.close()
 	except:
-		print('!!! SOME PROBLEM WITH SCAN OF',cidr,'!!!')
+		print('SOME PROBLEM WITH SCAN OF',ip_range)
 
 	subnet += 1
