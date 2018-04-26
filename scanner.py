ce40d5bc407b1e11b9ecec3e6e104902481882d6
Dane Morgan <danemorgan91@gmail.com>
2018-04-26 06:31:37 -0600

    cleaner code, better output formatting

--- scanner.py
+++ scanner.py
@@ -6,26 +6,36 @@ from datetime import datetime
 
 start = 192
 end = 255
+filename = 'test.csv'
 
 while start <= end:
 	raw_timestamp = time()
-	timestamp = datetime.fromtimestamp(raw_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-
+	timestamp = datetime.fromtimestamp(raw_timestamp).strftime('%H:%M:%S %d-%m-%Y')
 	nm = PortScanner()
-	subnet = '192.168.' + str(start) + '.0/24'
-	print('!!! Starting on:',ip_range,'!!!',timestamp)
 	
+	subnet = '192.168.' + str(start) + '.0/24'
+	init_msg = 'Starting on: ' + subnet + ' ' + timestamp + '\n'	
+	success_msg = 'Successfully scanned '+ subnet + ' ' + timestamp + '\n'
+	error_msg = 'Some problem scanning ' + subnet + ' ' + timestamp + '\n'
+
 	try:
 		#nmap -A -Pn 192.168.192.0/24
+		print(init_msg)
 		nm.scan(hosts=subnet, arguments='-A')
 		to_csv = nm.csv()
 
-		print('Successfully scanned',subnet,timestamp)
-		print(to_csv)
-		with open('net_scan.csv', 'a+') as f:
-			f.write(timestamp,'\n',to_csv)
+		with open(filename, 'a+') as f:
+			f.write(to_csv)
 			f.close()
+		print(success_msg,to_csv)
+	
+	except KeyboardInterrupt:
+		break
+		
 	except:
-		print('SOME PROBLEM WITH SCAN OF', subnet, timestamp)
+		with open(filename, 'a+') as f:
+			f.write(error_msg)
+			f.close()
+		print(error_msg)
 
 	start += 1
