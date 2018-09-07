abb8cc5905b4f347227be7dae890330a4f11c7d4
deadlift1226 <danemorgan91@gmail.com>
2018-09-07 10:01:42 -0600

    took out pandas

---
+++ scan_host.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#execute as root
+from time import time
+from nmap import PortScanner
+from datetime import datetime
+from cleaner import clean
+def scan(subnet,filename):
+    raw_timestamp = time()
+    timestamp = datetime.fromtimestamp(raw_timestamp).strftime('%H:%M:%S %d-%m-%Y')
+    nm = PortScanner()
+    #subnet = '192.168.' + str(start) + '.0/24'
+    init_msg = 'Starting on: ' + subnet + ' ' + timestamp + '\n'	
+    success_msg = 'Successfully scanned '+ subnet + ' ' + timestamp + '\n'
+    error_msg = 'Some problem scanning ' + subnet + ' ' + timestamp + '\n'
+
+    try:
+            #nmap -A -Pn 192.168.192.0/24
+            print(init_msg)
+            nm.scan(hosts=subnet, arguments='-A -Sv -Pn')
+            to_csv = nm.csv()
+
+            with open(filename, 'a+') as f:
+                    f.write(to_csv)
+                    f.close()
+            print(success_msg,to_csv)
+            #clean(filename)		
+    except KeyboardInterrupt:
+            #clean(filename)		
+            exit()   
+    except:
+            with open(filename, 'a+') as f:
+                    f.write(error_msg)
+                    f.close()
+            print(error_msg)	
+            #clean(filename)		
+
+subnet = '192.168.185.101-120' 
+filename = 'scan.csv'
+scan(subnet,filename)
+
